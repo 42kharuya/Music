b2f1a79edd10c05c80521ba4827e15b7586b61d5
42kharuya <haruya.0411.k@gmail.com>
2025-03-07 02:46:15 +0900

    Initial Commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by 川﨑遥也 on 2025/03/07.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicTests/MusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTests.swift
+//  MusicTests
+//
+//  Created by 川﨑遥也 on 2025/03/07.
+//
+
+import Testing
+@testable import Music
+
+struct MusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
